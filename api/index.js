21697d96871ed0ac01ea7d792d9e1b493e51e5d3
PhongVx <phongvxk38@gmail.com>
2018-07-22 11:02:23 +0700

    Push Live To Api

--- api/index.js
+++ api/index.js
@@ -4,11 +4,27 @@ const express=require('express');
 
 const PORT=3001;
 const app=express();
+app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 app.server=http.createServer(app);
 
 app.get('/',(req,res)=>{
     return res.json({version:'1.0'});
+})
+
+/*
+ * @method POST
+ * @endpoint /api/on-live-auth
+ * @description authentication for live stream user
+ * 
+*/
+
+app.post('/api/on-live-auth',(req,res,next)=>{
+    const streamInfo=req.body;
+    console.log(streamInfo);
+    return res.status(200).json({
+        verified:true,
+    })
 })
 
 app.server.listen(PORT,()=>{
