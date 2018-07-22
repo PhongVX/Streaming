de7c220db2025474bc8b36608e5713c7f21f04fd
PhongVx <phongvxk38@gmail.com>
2018-07-22 15:08:20 +0700

    Streaming key

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const http=require('http');
 const bodyParser=require('body-parser');
 const express=require('express');
+const _ = require('lodash');
 
 const PORT=3001;
 const app=express();
@@ -21,12 +22,20 @@ app.get('/',(req,res)=>{
 
 app.post('/api/on-live-auth',(req,res,next)=>{
     const streamInfo=req.body;
-    console.log(streamInfo);
+    const streamSecretKey= _.get(streamInfo,'name');
+    console.log(`User begin streaming and we are veryfing ${streamSecretKey}`);
     return res.status(200).json({
         verified:true,
     })
 })
-
+app.post('/api/on-live-done',(req,res,next)=>{
+    const streamSecretKey= _.get(req,'body.name');
+    console.log(`User finishing streaming camera`,streamSecretKey);
+    return res.json({
+        done:true
+    });
+    
+})
 app.server.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
-})
+})  
