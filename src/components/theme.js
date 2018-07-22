3e9d8e5f37b34cedd03fca30cac44b67fba7df4d
PhongVx <phongvxk38@gmail.com>
2018-07-22 10:07:43 +0700

    Build Server

--- src/components/theme.js
+++
@@ -1,6 +0,0 @@
-
-export const borderColor= 'rgba(0,0,0,0.07)';
-export const headerHeight=50;
-export const primaryColor='#2ecc71';
-export const dangerColor='#e67e22';
-export const containerMaxWidth=1200;
